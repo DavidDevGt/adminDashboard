c5d6e1d49961482b4eaa463037e0100d044d5137
Davidvl <josuedavidvl18@gmail.com>
2023-07-08 19:46:57 -0600

    Estructura inicial creada, first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'HTML Tutorial',
+        productNumber: '85712',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+        {
+        productName: 'Javascript Tutorial',
+        productNumber: '32398',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+        {
+        productName: 'FlexBox Full Course',
+        productNumber: '91233',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
